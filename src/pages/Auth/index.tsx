3a133b36123f41d2739d61b14032a9382018dfe0
Francisco Augusto <francisco.acs08@gmail.com>
2026-04-09 20:33:57 -0300

    feat: create initial auth page layout

---
+++ src/pages/Auth/index.tsx
@@ -0,0 +1,57 @@
+function Auth() {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="hiden md:flex md:w-1/2 lg:w-[60%] bg-[#012169]  flex-col justify-center p-12 pr-15 items-start gap-8">
+        <h1 className="text-white text-4xl font-bold  ">
+          The Carreer Architect
+        </h1>
+        <h2 className="text-white text-5xl">
+          Sua jornada profissional começa aqui
+        </h2>
+        <p className="text-[#82A0F5] text-2xl ">
+          Projete seu futuro com ferramentas de design editorial que transforma
+          sua trajetória em uma narrativa de autoridade.
+        </p>
+        <div className="flex gap-16">
+          <div>
+            <p className="font-bold text-5xl text-green-400">500k+</p>
+            <p className="text-[#82A0F5] font bold uppercase mt-3 opacity-80">
+              carreiras Construídas
+            </p>
+          </div>
+          <div>
+            <p className="font-bold text-5xl text-green-400">98%</p>
+            <p className="text-[#82A0F5] font bold uppercase mt-3 opacity-80">
+              Taxa de Sucesso
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-center items-center p-12">
+        <div className="w-full max-w-md flex flex-col gap-8">
+          <div className="bg-[#E6E6EB] p-1.5 rounded-2xl flex shadow-sm self-center">
+            <button className="flex-1 py-4 px-8 m-1 rounded-xl font-bold hover:scale-105 cursor-pointer transition-all duration-200 bg-white text-black shadow-md">
+              Entrar
+            </button>
+            <button className="flex-1 py-4 px-8 m-1 rounded-xl font-bold hover:scale-105 cursor-pointer transition text-gray-500">
+              Criar Conta
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-4 text-left">
+            <h1 className="text-black text-5xl font-bold leading-tight">
+              Bem-vindo de volta
+            </h1>
+
+            <p className="text-gray-600 text-lg leading-relaxed">
+              Insira suas credenciais para acessar sua conta profissional
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
